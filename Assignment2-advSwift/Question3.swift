72e69f4fa6de111dfc7705a35116d0550d51a0eb
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-06-14 22:16:04 +0100

    added a laptop class

--- Assignment2-advSwift/Question3.swift
+++ Assignment2-advSwift/Question3.swift
@@ -7,6 +7,39 @@
 
 import Foundation
 
+/**
+ create a parent class and a subclass
+ 
+ PARENT CLASS:
+ class name:
+ - Computer
+ attributes:
+ - cpu: String
+ - gpu: String
+ - ram: Int
+ - state: String = "Off"
+ - temp: Double
+ methods:
+ - init(cpu: String, gpu: String, ram: Int, temp: Double)
+ - upgradeRam(newAmount: Int)
+ - turnOn() => will set the state to 'On'
+ - turnOff() => will set the state to 'Off'
+ - getTemp() -> Double
+ 
+ 
+ CHILD CLASS:
+ class name:
+ - Laptop
+ attributes:
+ - isRamUpgradable: Bool
+ - screenSize: Double
+ - isTouchScreen: Bool
+ methods:
+ - init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool)
+ - override upgradeRam() => if the ram is not upgradable do nothing
+ - closeScreen() => this will set the laptop state to 'Sleep'
+ */
+
 class Computer {
     var cpu: String
     var gpu: String
@@ -35,5 +68,30 @@ class Computer {
     
     func getTemp() -> Double {
         return temp
+    }
+}
+
+class Laptop: Computer {
+    var isRamUpgradable: Bool
+    var screenSize: Double
+    var isTouchScreen: Bool
+    
+    init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool) {
+        self.isRamUpgradable = isRamUpgradable
+        self.screenSize = screenSize
+        self.isTouchScreen = isTouchScreen
+        
+        super.init(cpu: cpu, gpu: gpu, ram: ram, temp: temp)
+        self.state = state
+    }
+    
+    override func upgradeRam(newAmount: Int) {
+        if isRamUpgradable {
+            super.upgradeRam(newAmount: newAmount)
+        }
+    }
+    
+    func closeScreen() {
+        state = "Sleep"
     }
 }
