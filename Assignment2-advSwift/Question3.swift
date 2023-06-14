c211aefb83bfd73851c0a30f8ea8e3fb9cc6d0cf
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-06-14 22:05:51 +0100

    created computer class

--- Assignment2-advSwift/Question3.swift
+++ Assignment2-advSwift/Question3.swift
@@ -6,35 +6,34 @@
 //
 
 import Foundation
-/**
- create a parent class and a subclass
- 
- PARENT CLASS:
- class name:
- - Computer
- attributes:
- - cpu: String
- - gpu: String
- - ram: Int
- - state: String = "Off"
- - temp: Double
- methods:
- - init(cpu: String, gpu: String, ram: Int, temp: Double)
- - upgradeRam(newAmount: Int)
- - turnOn() => will set the state to 'On'
- - turnOff() => will set the state to 'Off'
- - getTemp() -> Double
- 
- 
- CHILD CLASS:
- class name:
- - Laptop
- attributes:
- - isRamUpgradable: Bool
- - screenSize: Double
- - isTouchScreen: Bool
- methods:
- - init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool)
- - override upgradeRam() => if the ram is not upgradable do nothing
- - closeScreen() => this will set the laptop state to 'Sleep'
- */
+
+class Computer {
+    var cpu: String
+    var gpu: String
+    var ram: Int
+    var state: String = "Off"
+    var temp: Double
+    
+    init(cpu: String, gpu: String, ram: Int, temp: Double) {
+        self.cpu = cpu
+        self.gpu = gpu
+        self.ram = ram
+        self.temp = temp
+    }
+    
+    func upgradeRam(newAmount: Int) {
+        ram = newAmount
+    }
+    
+    func turnOn() {
+        state = "On"
+    }
+    
+    func turnOff() {
+        state = "Off"
+    }
+    
+    func getTemp() -> Double {
+        return temp
+    }
+}
